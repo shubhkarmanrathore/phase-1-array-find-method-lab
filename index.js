274f3fe9f8cb2211ba1ba0b9d396a228f907b338
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-03-28 19:11:15 -0700

    adds array find lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+function superbowlWin(record) {
+  const winningRecord = record.find(record => record.result === "W");
+  return winningRecord ? winningRecord.year : undefined;
+}
+
+// function superbowlWin(record) {
+//   const winningRecord = record.find(function(record) {
+//     return record.result === "W";
+//   });
+  
+//   if (winningRecord) {
+//     return winningRecord.year;
+//   } else {
+//     return undefined;
+//   }
+// }
+
+// function superbowlWin(record) {
+//   let winningYear;
+  
+//   record.forEach(function(game) {
+//     if (game.result === "W") {
+//       winningYear = game.year;
+//     }
+//   });
+  
+//   return winningYear;
+// }
